26c80054012aa7e5c3f9a5d0af72fa6bc05f3f2f
Makar Selivanov <makar.selivanov@gmail.com>
2023-09-24 13:51:08 +0300

    add database

--- database/database.go
+++ database/database.go
@@ -1,5 +1,7 @@
 package database
 
+import "log"
+
 var globalVersion int = 0
 
 type Snapshot struct {
@@ -21,13 +23,50 @@ func NewSnapshot(newValue string) Snapshot {
 	globalVersion++
 	return Snapshot{
 		value:   newValue,
-		version: globalVersion - 1,
+		version: globalVersion,
 	}
 }
 
 func NewDatabase() Database {
+	log.Printf("Create database")
 	return Database{
 		snapshot:     NewSnapshot(""),
 		transactions: make([]Transaction, 0),
+	}
+}
+
+func (db Database) AddTransaction(value string) {
+	curVersion := db.snapshot.version
+	transaction := Transaction{
+		fromSnapshotVersion: curVersion,
+		value:               value,
+	}
+	//FIXME not thread safe?
+	db.transactions = append(db.transactions, transaction)
+	log.Println("Add new transaction from snapshot version %d", curVersion)
+}
+
+func (db Database) GetValue() string {
+	//FIXME should be tread safe?
+	shapshot := db.snapshot
+	value := shapshot.value
+	if len(db.transactions) > 0 && db.transactions[len(db.transactions)-1].fromSnapshotVersion >= shapshot.version {
+		value = db.transactions[len(db.transactions)-1].value
+		log.Println("Last value from transaction")
+	} else {
+		log.Println("Last value from snapshot")
+	}
+	log.Println("Return value from database: %s", value)
+	return value
+}
+
+func (db Database) SaveSnapshot() {
+	if len(db.transactions) == 0 {
+		return
+	}
+	operation := db.transactions[len(db.transactions)-1]
+	if operation.fromSnapshotVersion >= globalVersion {
+		db.snapshot = NewSnapshot(operation.value)
+		log.Println("Save new snapshot version %d", db.snapshot.version)
 	}
 }
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	database "Database"
+	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
 var db = database.NewDatabase()
@@ -11,7 +13,16 @@ var db = database.NewDatabase()
 func requestReplace(writer http.ResponseWriter, reader *http.Request) {
 	switch reader.Method {
 	case http.MethodPost:
-		//TODO
+		log.Println("POST /replace")
+		var buffer string
+		_, err := reader.Body.Read([]byte(buffer))
+
+		if err != nil && err != io.EOF {
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		writer.WriteHeader(http.StatusOK)
+		db.AddTransaction(buffer)
 	default:
 		writer.WriteHeader(http.StatusBadRequest)
 	}
@@ -20,13 +31,23 @@ func requestReplace(writer http.ResponseWriter, reader *http.Request) {
 func requestGet(writer http.ResponseWriter, reader *http.Request) {
 	switch reader.Method {
 	case http.MethodGet:
-		//TODO
+		log.Println("GET /get")
+		writer.WriteHeader(http.StatusOK)
+		writer.Write([]byte(db.GetValue()))
 	default:
 		writer.WriteHeader(http.StatusBadRequest)
 	}
 }
 
+func autoSaveSnapshot() {
+	for {
+		time.Sleep(time.Minute)
+		db.SaveSnapshot()
+	}
+}
+
 func main() {
+	go autoSaveSnapshot()
 	http.HandleFunc("/replace", requestReplace) // Устанавливаем роутер
 	http.HandleFunc("/get", requestGet)
 	err := http.ListenAndServe(":8080", nil) // устанавливаем порт веб-сервера
